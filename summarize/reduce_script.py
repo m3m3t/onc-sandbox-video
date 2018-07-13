155cf92111be4ebf7aae316b9ab0de5c51fde615
Amanda Dash <memet666@gmail.com>
2018-07-13 14:44:40 -0700

    Added comments and delete pch explicitly to prevent possible memory leaks

--- summarize/reduce_script.py
+++ summarize/reduce_script.py
@@ -1,3 +1,10 @@
+
+#!/usr/env/python3
+"""
+
+This script is demonstration of the ONC Oceans 2.0 sandbox.  The program will access video files from an underwater video camera and reduce the videos duration by roughly 50 percent to save on space and watch time. The videos are reduced based on "novelty" which is modelled using background subtraction and historical repetition.
+"""
+
 from onc.onc import ONC
 
 import imageio
@@ -9,13 +16,21 @@ import sys
 
 from pch import PCH
 
+"""
+Run through the video and only save frames that exhibit
+a certain amount of novelty that modelled using the PCH
+algorithm.
+"""
 def runVideo(video_fn, video_out_fn, opts, debug=False):
     video = imageio.get_reader(video_fn,  'ffmpeg')
 
+    #To speed up processing, we resize the video to smaller
+    #dimensions
     frame_size = (608,800) 
     fps = video.get_meta_data()['fps']
     nframes = video.get_meta_data()['nframes']
 
+    #We'll use the same frames per second as the input video
     video_out = imageio.get_writer(video_out_fn, fps=fps)
 
     print(video._meta)
@@ -23,9 +38,12 @@ def runVideo(video_fn, video_out_fn, opts, debug=False):
     gray_curr = None
     gray_prev = None 
 
+    #Create our novelty model
     pch = PCH(**opts["pch"])
     pch.initialize(frame_size, fps)
 
+    #Another time saving measure, we're only going to sample
+    #some of the videos for analysis
     sampledFrames = np.arange(0,nframes, opts['samplingRate'])
         
     frame_color = video.get_data(0)
@@ -42,13 +60,16 @@ def runVideo(video_fn, video_out_fn, opts, debug=False):
         gray_curr = cv.resize(frame_color, (frame_size[1],frame_size[0])) 
         gray_curr = cv.cvtColor(gray_curr, cv.COLOR_RGB2GRAY) 
 
+        #Given the underwater is murky, we'll only consider high novel events (255). 
         result = pch.update_model(gray_prev, gray_curr)
         result[result < 255] = 0
         new_motion = np.count_nonzero(result)
 
         gray_prev = gray_curr
 
+        #To prevent noise, we'll use a threshold for the number of events. Since we always resize videos, we can use a set value
         if new_motion > opts["sampleThreshold"]: 
+            #To prevent video "fast forwarding" we'll go back and write the frames that we skipped during sampling.  This adds a bit of time back but still better than not sampling.
             from_frame = frame_num - opts["samplingRate"]
             for num in np.arange(from_frame, frame_num):
                 video_out.append_data(video.get_data(num))
@@ -56,7 +77,8 @@ def runVideo(video_fn, video_out_fn, opts, debug=False):
         
         if frame_num % int(fps)*5 == 0:
             print("- {} seconds done.".format(frame_num / fps))
-        
+       
+        #Show video during debugging locally
         if debug:
             cv.imshow("result", result)
             cv.imshow("frame", gray_curr)
@@ -64,8 +86,12 @@ def runVideo(video_fn, video_out_fn, opts, debug=False):
 
     video_out.close()
     video.close()
-   
+ 
+    #To prevent any memory leaks
+    del pch
 
+#Load user defined options from a json file 
+print("INFO: Loading user settings from json file")
 with open("params.json", "r") as f:
     params = json.load(f)
 
@@ -73,12 +99,15 @@ with open("params.json", "r") as f:
     search_ = params["search"]
     opts    = params["summarize"]
     
-    from onc.onc import ONC
+    print("INFO: Connecting to ONC Oceans 2.0")
     onc = ONC(onc_["token"], 
               onc_["production"],
               onc_["showInfo"], 
               onc_["outPath"])
 
+    #Replace this value(s) with the desired video feeds 
+    #you want to examine.
+    print("INFO: Performing data queries")
     dps = onc.getDataProducts(filters={
                 'deviceCode'   : search_["deviceCode"], 
                 'locationCode' : search_["locationCode"], 
@@ -91,6 +120,8 @@ with open("params.json", "r") as f:
         'deviceCode'        : search_["deviceCode"],
     }
 
+    #We don't want to download at once in case we have a large amount of video files, so we just retrieve the url files
+    print("INFO: Retrieve the data product info")
     orders = onc.orderDataProduct(
                 query, 
                 onc_["maxRetries"], 
@@ -101,10 +132,15 @@ with open("params.json", "r") as f:
     for url,video_fn in toDownload:
         result = onc.downloadFile(url)
         print("{} downloaded: {}".format(video_fn, result['downloaded']))
+       
+        print("{}: Reducing video to important events".format(video_fn))
         video_out_fn = video_fn.replace("." + search_["extension"], "_summ." + search_["extension"])
         runVideo(video_fn, video_out_fn, opts)
+       
+        #Most of the video is not important, so we don't want to keep the original video to save space.
         if not opts["keepOriginal"]:
             shutil.move(video_out_fn, video_fn)
-            print("Removing original file ", video_fn)
-        print("{} finished.".format(video_fn))
-        
+            print("{}: Removing original file ".format(video_fn))
+        print("{}:  Finished.".format(video_fn))
+
+print("INFO: Script finished")
